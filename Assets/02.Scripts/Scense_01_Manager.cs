c40587593103f73944d949bd2ae283a63b9bfa77
102real <reggae1@naver.com>
2021-09-04 15:13:19 +0900

    eff 추가

---
+++ Assets/02.Scripts/Scense_01_Manager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scense_01_Manager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
