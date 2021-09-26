0a6d9ecb239dab4adeb525cd281e7ee69fc5b51b
102real <reggae1@naver.com>
2021-09-26 10:53:30 +0900

    코루틴업

--- Assets/02.Scripts/Scense_02_Manager.cs
+++ Assets/02.Scripts/Scense_02_Manager.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class Scense_02_Manager : MonoBehaviour
 {
-    [SerializeField]
-    GameObject player;
+    public AudioSource Play2_1;
+    public AudioSource Play2_2;
+    public AudioSource Play2_3;
+    public AudioSource Play2_4;
+    public AudioSource Play2_5;
+    public AudioSource Play2_6;
+    public AudioSource Play2_7;
+    public AudioSource Play2_8;
+    public AudioSource Play2_9;
+    public AudioSource Play2_10;
+    public AudioSource Play2_11;
+    public AudioSource Play2_12;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine("SoundC1");
+        }
+    }
+
+
+    IEnumerator SoundC1()
+    {
+        Debug.Log("시작");
+        Play2_1.Play();
+        yield return new WaitForSeconds(4f);
+        Debug.Log("4초 지남");
     }
 }
